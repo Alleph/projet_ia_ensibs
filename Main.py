bfb72bd9378bf259fb0d24850e80c4ef0094ee23
Alleph (Enzo) <denoue.enzo@gmail.com>
2023-12-22 19:10:10 +0100

    Add argv system and help

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from SearchingFunctions import SearchingFunctions
 from Drawer import Drawer
 from Classifier import Classifier
 import os
+import sys
 import pprint
 import glob
 import time
@@ -21,12 +22,57 @@ CRT_PATH = "certs/http_ca.crt"
 # List of xml files to index
 XML_DIR = "TRAIN_ENSIBS"
 XML_FILES = glob.glob(os.path.join(XML_DIR, "*.xml"))
-XML_TEST_HTTPWeb = "TEST_ENSIBS/benchmark_HTTPWeb_test.xml"
-XML_TEST_SSH = "TEST_ENSIBS/benchmark_SSH_test.xml"
+XML_TEST_HTTPWeb = "defi1/benchmark_HTTPWeb_test.xml"
+XML_TEST_SSH = "defi1/benchmark_SSH_test.xml"
 XML_TEST_DEFI2 = "defi2/traffic_os_TEST.xml"
 XML_TRAIN_DEFI2 = "defi2/traffic_os_TRAIN.xml"
 
+# List of command line arguments
+
 def main():
+    ###------------------------------------------------------------------------------------------###
+    ###--------------------------------------- Arguments ----------------------------------------###
+    ###------------------------------------------------------------------------------------------###
+
+    if len(sys.argv) < 2 or  sys.argv[1] == "-h":
+        print("""\nUSAGE : python3 Main.py [project|defi1|defi2] [appName] [KNN|MNB]""")
+        print("""\nOPTIONAL :""")
+        print("""|  -index    To index all the XML files in TRAIN_ENSIBS.""")
+        print("""|  -delete   To delete all indexes.""")
+        print("""|  -vect     To convert flows to vectors and save them in a file. (Must be used only once)""")
+        print("""|  KNN 4     To use KNN classifier with k=4 (default k=1)""")
+        print("""|  -h        To display help.""")
+        print("""\nEXAMPLES : """)
+        print("""|  python3 Main.py project HTTPWeb KNN 3""")
+        print("""|  python3 Main.py defi1 SSH MNB -vect""")
+        sys.exit(1)
+
+    if sys.argv[1] not in ["project", "defi1", "defi2", "-index", "-delete"]:
+        print("""Wrong argument : Please enter "project", "defi1" of "defi2" followed by the appName and the classifier type (KNN or MNB).""")
+        print("EXAMPLES : ")
+        print("|  python3 Main.py project HTTPWeb KNN 3")
+        print("|  python3 Main.py defi1 SSH MNB")
+        sys.exit(1)
+
+    appList = ['HTTPWeb', 'HTTPImageTransfer', 'DNS', 'Unknown_UDP', 'SecureWeb', 'Unknown_TCP', 'NetBIOS-IP', 'POP', 'WindowsFileSharing', 'FTP', 'IMAP', 'BitTorrent', 'SSH', 'ICMP', 'SMTP', 'WebMediaDocuments', 'Flowgen', 'MiscApplication', 'WebFileTransfer', 'XWindows', 'Oracle', 'WebMediaVideo', 'Yahoo', 'Authentication', 'Real', 'RPC', 'Telnet', 'IRC', 'Filenet', 'Webmin', 'DNS-Port', 'MSMQ', 'MSN', 'IPSec', 'Timbuktu', 'H.323', 'Common-P2P-Port', 'AOL-ICQ', 'Web-Port', 'MS-SQL', 'MSN-Zone', 'MSTerminalServices', 'StreamingAudio', 'IGMP', 'Hotline', 'Misc-DB', 'LDAP', 'NNTPNews', 'SNMP-Ports', 'NETBEUI', 'WebMediaAudio', 'SMS', 'PPTP', 'SSDP', 'Common-Ports', 'RTSP', 'SunRPC', 'VNC', 'Tacacs', 'Gnutella', 'MDQS', 'ManagementServices', 'TimeServer', 'Citrix', 'Ingres', 'NFS', 'NortonAntiVirus', 'Anet', 'Groove', 'NTP', 'Squid', 'Misc-Ports', 'MicrosoftMediaServer', 'MiscApp', 'BGP', 'Hosts2-Ns', 'Misc-Mail-Port', 'OpenNap', 'POP-port', 'XFER', 'rsh', 'Google', 'Intellex', 'Network-Config-Ports', 'PCAnywhere', 'PostgreSQL', 'Printer', 'SSL-Shell', 'dsp3270', 'rexec', 'rlogin', 'OpenWindows', 'PeerEnabler', 'SAP', 'NortonGhost', 'SNA', 'Nessus', 'IPX', 'iChat', 'Kazaa']
+
+    if sys.argv[1] in ["project", "defi1"] and sys.argv[2] not in appList:
+        print("""Wrong appName : Please enter an appName among the list bellow, followed the classifier type (KNN or MNB).""")
+        for appName in appList:
+            print(f"- {appName}")
+        print("EXAMPLES : ")
+        print("|  python3 Main.py project HTTPWeb KNN 3")
+        print("|  python3 Main.py defi1 SSH MNB")
+        sys.exit(1)
+
+    if sys.argv[1] in ["project", "defi1"] and sys.argv[2] in appList and sys.argv[3] not in ["KNN", "MNB"]:
+        print("""Wrong classifier type : Please enter KNN or MNB after appName.""")
+        print("EXAMPLES : ")
+        print("|  python3 Main.py project HTTPWeb KNN 3")
+        print("|  python3 Main.py defi1 SSH MNB")
+        sys.exit(1)
+
+
     ###------------------------------------------------------------------------------------------###
     ###----------- Start indexation and initialize researcher, drawer and converter -------------###
     ###------------------------------------------------------------------------------------------###
@@ -40,16 +86,24 @@ def main():
         ELASTIC_USERNAME,
         ELASTIC_PASSWORD)
 
-    # Delete all indexes at the beginning
-    #bulk_indexer.delete_all_indexes()
+    if "-delete" in sys.argv:
+        # Delete all indexes at the beginning
+        bulk_indexer.delete_all_indexes()
+        print("\nPlease restart the program to index the XML files with -index.\n")
 
-    # Index all the XML files
-    #for xml_file in XML_FILES:
-    #    bulk_indexer.bulk_index_data(xml_file)
+        sys.exit(1)
+
+    if "-index" in sys.argv:
+
+        # Index all the XML files
+        for xml_file in XML_FILES:
+            bulk_indexer.bulk_index_data(xml_file)
     
-    # Index only one XML file
-    #xml_file = XML_FILES[-1]
-    #bulk_indexer.bulk_index_data(xml_file)
+        # Index only one XML file
+        #xml_file = XML_FILES[-1]
+        #bulk_indexer.bulk_index_data(xml_file)
+
+        sys.exit(1)
 
     # Update the max result window size
     bulk_indexer.update_max_result_windows("flows", 200000)
@@ -100,7 +154,7 @@ def main():
 
     # If you want to get the number of flows for each application
     #nb_flows_per_application = sf.get_nb_flows_for_each_application()
-    #pprint.pprint(nb_flows_per_application)    
+    #pprint.pprint(nb_flows_per_application)
 
     # If you want to get the list of flows for a given application
     #application = "HTTPWeb"
@@ -190,27 +244,41 @@ def main():
     ###------------------------------------------------------------------------------------------###
     ###-------------------------------- Classification preparation ------------------------------###
     ###------------------------------------------------------------------------------------------###
-    print("--- Classification preparation ---")
 
-    files = ['binarized_flows/binarized_flows_test_1.pickle', 'binarized_flows/binarized_flows_test_2.pickle',
-              'binarized_flows/binarized_flows_test_3.pickle', 'binarized_flows/binarized_flows_test_4.pickle',
-              'binarized_flows/binarized_flows_test_5.pickle']
+    files = None
 
-    appName = "SSH"
+    if sys.argv[1] == "project":
 
-    #files = f"defi1/binarized_train_flows/{appName}_binarized_train_flows.pickle"
+        files = ['binarized_flows/binarized_flows_test_1.pickle', 'binarized_flows/binarized_flows_test_2.pickle',
+                'binarized_flows/binarized_flows_test_3.pickle', 'binarized_flows/binarized_flows_test_4.pickle',
+                'binarized_flows/binarized_flows_test_5.pickle']
 
-    # Prepare classification for the HTTPWeb protocol.
-    # Please comment out this line after binarization (for the first time only).
-    #class_prep(appName, sf, cv, files, True)
+        # Prepare classification for the HTTPWeb protocol.
+        print("---------- Classification preparation ----------")
+        
+        if len(sys.argv) > 4 and "-vect" in sys.argv:
+            class_prep(appName, sf, cv, files, True)
+
+    appName = sys.argv[2]
+
+    if sys.argv[1] == "defi1":
+
+        files = f"defi1/binarized_train_flows/{appName}_binarized_train_flows.pickle"
+
+        # Prepare classification for the HTTPWeb protocol.
+        print("---------- Classification preparation ----------")
+
+        if len(sys.argv) > 4 and "-vect" in sys.argv:
+            class_prep(appName, sf, cv, files, True)
 
     ###------------------------------------------------------------------------------------------###
     ###-------------------------------------- Classification ------------------------------------###
     ###------------------------------------------------------------------------------------------###
 
-    print("--- Classification ---")
+    if sys.argv[1] == "project" or sys.argv[1] == "defi1":
+        print("---------- Classification ----------")
     
-    classifier_type = "MNB" # "KNN" or "MNB" for KNN classifier or Multinomial Naive Bayes classifier
+        classifier_type = sys.argv[3] # "KNN" or "MNB" for KNN classifier or Multinomial Naive Bayes classifier
 
     if isinstance(files, list):
         # Get subsets from files
@@ -225,12 +293,18 @@ def main():
 
         if classifier_type == "KNN":
             # Predict attack flows with KNN classifier
-            csf.classifyKNN(k=6)
+            if len(sys.argv) > 4 and sys.argv[4].isdigit():
+                k = int(sys.argv[4])
+            csf.classifyKNN(k=k)
         elif classifier_type == "MNB":
             # Predict attack flows with Multinomial Naive Bayes classifier
             csf.classifyMNB()
 
-    else:
+    ###------------------------------------------------------------------------------------------###
+    ###---------------------------------------- Testing defi 1 ----------------------------------###
+    ###------------------------------------------------------------------------------------------###
+    elif sys.argv[1] == "defi1":
+        print("---------- Defi 1 ----------")
         # Get vectors from files
         training_vectors = read_subsets_from_files(files)
         print("- Training vectors for defi 1 loaded from file.")
@@ -238,64 +312,76 @@ def main():
         # Init the classifier
         csf = Classifier(training_vectors)
 
-    ###------------------------------------------------------------------------------------------###
-    ###---------------------------------------- Testing defi 1 ----------------------------------###
-    ###------------------------------------------------------------------------------------------###
+        if len(sys.argv) > 4 and "-vect" in sys.argv:
+            
+            # Prepare testing flows for the protocol.
+            if appName == "HTTPWeb":
+            
+                xml_parser = XMLParser(XML_TEST_HTTPWeb)
+                xml_parser.load_xml2()
+                testing_flows = xml_parser.get_flows()
 
-    # Prepare testing flows for the HTTPWeb protocol.
-    
-    # xml_parser = XMLParser(XML_TEST_HTTPWeb)
-    # xml_parser.load_xml2()
-    # testing_flows = xml_parser.get_flows()
+            elif appName == "SSH":
 
-    # print("Testing flows for defi 1 loaded from file.")
-    # print("Number of testing flows : ", len(testing_flows))
+                xml_parser = XMLParser(XML_TEST_SSH)
+                xml_parser.load_xml2()
+                testing_flows = xml_parser.get_flows()
 
-    # # Convert flows to vectors
-    # testing_vectors = flows_to_vector(testing_flows, cv)
-    # print("Testing vector loaded from file.")
-    # print("Number of testing vectors : ", len(testing_vectors))
+            else:
+                print("Wrong appName for defi1 : Please enter HTTPWeb or SSH appName.")
+                sys.exit(1)
 
-    # # Write testing vector on file
-    # write_test_vectors_on_file(testing_vectors, cv, f"defi1/binarized_test_flows/{appName}_binarized_test_flows.pickle")
+            print("Testing flows for defi 1 loaded from xml file.")
+            print("Number of testing flows : ", len(testing_flows))
 
-    # # Load testing vector from file
-    # testing_vectors = read_subsets_from_files(f"defi1/binarized_test_flows/{appName}_binarized_test_flows.pickle")
-    # print("- Testing vectors loaded from file.")
+            # Convert flows to vectors
+            testing_vectors = flows_to_vector(testing_flows, cv)
+            print("Testing flows converted into testing vectors succesfully.")
+            print("Number of testing vectors : ", len(testing_vectors))
 
-    # classifier_type = "KNN" # "KNN" or "MNB" for KNN classifier or Multinomial Naive Bayes classifier
+            # Write testing vector on file
+            write_test_vectors_on_file(testing_vectors, cv, f"defi1/binarized_test_flows/{appName}_binarized_test_flows.pickle")
+        
+        if "KNN" in sys.argv and len(sys.argv) > 4 and sys.argv[4].isdigit():
+            k = int(sys.argv[4])
 
-    # # Predict attack flows with KNN classifier
-    # proba = csf.predict_attack(training_vectors, testing_vectors, classifier_type, k=6)
+        # Load testing vector from file
+        testing_vectors = read_subsets_from_files(f"defi1/binarized_test_flows/{appName}_binarized_test_flows.pickle")
+        print("Testing vectors loaded from file.")
 
-    # # Make json file of results
-    # csf.make_json_res(proba, classifier_type, appName)
+        # Predict attack flows with KNN classifier
+        proba = csf.predict_attack(training_vectors, testing_vectors, classifier_type, k=k)
 
+        # Make json file of results
+        csf.make_json_res(proba, classifier_type, appName)
 
     ###------------------------------------------------------------------------------------------###
     ###------------------------------------------ Defi 2 ----------------------------------------###
     ###------------------------------------------------------------------------------------------###
-    # print("------------ Defi 2 ------------")
+    
+    if sys.argv[1] == "defi2":
+    
+        print("---------- Defi 2 ----------")
 
-    # # Prepare training flows.
-    # xml_parser = XMLParser(XML_TRAIN_DEFI2)
-    # xml_parser.load_xml()
-    # training_flows = xml_parser.get_flows()
+        if "-vect" in sys.argv:
+            # Prepare training flows.
+            xml_parser = XMLParser(XML_TRAIN_DEFI2)
+            xml_parser.load_xml()
+            training_flows = xml_parser.get_flows()
 
-    # print("Training flows for defi 2 loaded from file.")
-    # print("Number of training flows : ", len(training_flows))
+            print("Training flows for defi 2 loaded from xml file.")
+            print("Number of training flows : ", len(training_flows))
 
-    # # Prepare testing flows.
-    # xml_parser = XMLParser(XML_TEST_DEFI2)
-    # xml_parser.load_xml()
-    # testing_flows = xml_parser.get_flows()
+            # Prepare testing flows.
+            xml_parser = XMLParser(XML_TEST_DEFI2)
+            xml_parser.load_xml()
+            testing_flows = xml_parser.get_flows()
 
-    # print("Testing flows for defi 2 loaded from file.")
-    # print("Number of testing flows : ", len(testing_flows))
+            print("Testing flows for defi 2 loaded from xml file.")
+            print("Number of testing flows : ", len(testing_flows))
 
-    # # Convert flows to vectors
-    # print("First flow of testing vectors : ", testing_flows[:3])
-    # print("First flow of training vectors : ", training_flows[:3])
+        print("First flow of testing vectors : ", testing_flows[0])
+        print("First flow of training vectors : ", training_flows[0])
 
 
 if __name__ == "__main__":
